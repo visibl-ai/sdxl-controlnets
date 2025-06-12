06c7f1527a97c480afa47d8c4c8083fc51c365e5
Moe Adham <moeadham@gmail.com>
2025-06-12 11:49:05 +0900

    fix run

--- install.sh
+++ install.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 python3 -m venv .venv
 source .venv/bin/activate
-source .env
-if [ -z "$HF_TOKEN" ]; then
-    echo "Error: HF_TOKEN environment variable is not set"
-    exit 1
-fi
-export HF_HOME=./cache
-export HUGGINGFACE_HUB_CACHE=hub
-export TRANSFORMERS_CACHE=transformers
-export HF_DATASETS_CACHE=datasets
 pip install --cache-dir=.venv/pip-cache -r requirements.txt
 echo "Running once to download all models"
-python sd3.5_diffusers_control.py
+./run_interactive.sh
--- run_interactive.sh
+++ run_interactive.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+source .env
+if [ -z "$HF_TOKEN" ]; then
+    echo "Error: HF_TOKEN environment variable is not set."
+    echo "Please create a .env file in the current directory with the following content:"
+    echo "export HF_TOKEN=\"your huggingface token\""
+    exit 1
+fi
+export HF_HOME=./cache
+export HUGGINGFACE_HUB_CACHE=hub
+export TRANSFORMERS_CACHE=transformers
+export HF_DATASETS_CACHE=datasets
 source .venv/bin/activate
 python sd3.5_diffusers_control.py
