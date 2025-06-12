b8b79a597efde32a8dfd4cbc305cf7ab80f83009
Moe Adham <moeadham@gmail.com>
2025-06-12 13:42:33 +0900

    better depth model

--- src/config.py
+++ src/config.py
@@ -18,7 +18,7 @@ class Config:
         self.blur_controlnet_path = "moeadham/stable-diffusion-3.5-large-controlnet-blur-diffusers"
         
         # Depth model configuration
-        self.depth_model_type = os.environ.get("DEPTH_MODEL_TYPE", "dpt")  # Options: "dpt" or "depth_anything_v2"
+        self.depth_model_type = os.environ.get("DEPTH_MODEL_TYPE", "depth_anything_v2")  # Options: "dpt" or "depth_anything_v2"
         self.depth_model = "Intel/dpt-hybrid-midas"  # Used when depth_model_type is "dpt"
         self.depth_anything_model = "depth-anything/Depth-Anything-V2-Large-hf"  # Used when depth_model_type is "depth_anything_v2"
         
